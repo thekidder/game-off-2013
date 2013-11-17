7e9186ad696aa3bce72cda0a89e66ddc6b7f30e0
Adam Kidder <thekidder@gmail.com>
2013-11-17 01:51:37 -0800

    added button to rebuild tower

--- unity/Assets/TowerGen/Editor/SeedEditor.cs
+++ unity/Assets/TowerGen/Editor/SeedEditor.cs
@@ -10,12 +10,18 @@ public class SeedEditor : PropertyDrawer
 
         position = EditorGUI.PrefixLabel (position, GUIUtility.GetControlID (FocusType.Passive), label);
 
-        Rect fieldRect = new Rect (position.x, position.y, position.width - 82, position.height);
-        Rect buttonRect = new Rect (position.x + position.width - 80, position.y, 80, position.height);
+        Rect fieldRect = new Rect (position.x, position.y, position.width - 82 - 62, position.height);
+        Rect regenRect = new Rect (position.x + position.width - 80, position.y, 80, position.height);
+        Rect rebuildRect = new Rect (position.x + position.width - 82 - 60, position.y, 60, position.height);
 
         property.FindPropertyRelative ("seed").intValue = EditorGUI.IntField (fieldRect, property.FindPropertyRelative ("seed").intValue);
 
-        if (GUI.Button (buttonRect, "Randomize")) {
+        if (GUI.Button(rebuildRect, "Rebuild")) {
+            GUI.FocusControl(null);
+            property.FindPropertyRelative("oldSeed").intValue = 0;
+        }
+
+        if (GUI.Button (regenRect, "Randomize")) {
             GUI.FocusControl (null);
             property.FindPropertyRelative ("seed").intValue = Random.Range (0, System.Int32.MaxValue);
         }
