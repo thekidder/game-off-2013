120bc4dee8eed6338f1cc0e123797c16937f796f
Adam Kidder <thekidder@gmail.com>
2013-11-17 01:52:01 -0800

    make tower gen deterministic based on seed
    use unity random in Shuffle

--- unity/Assets/Utils.cs
+++ unity/Assets/Utils.cs
@@ -1,15 +1,14 @@
-﻿using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 static public class Utils
 {
     public static void Shuffle<T> (this List<T> list)
     {  
-        Random rng = new Random ();  
         int n = list.Count;  
         while (n > 1) {  
-            n--;  
-            int k = rng.Next (n + 1);  
+            n--;
+            int k = Random.Range(0, n + 1);
             T value = list [k];  
             list [k] = list [n];  
             list [n] = value;  
